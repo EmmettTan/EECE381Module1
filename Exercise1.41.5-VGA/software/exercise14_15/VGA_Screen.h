efb0b41af20d181a2e57fe6ad9dc11838acf8f91
EmmettTan <UBCEmmettTan@gmail.com>
2014-10-01 12:03:39 -0700

    VGA_Screen class can take a 11x11 matrix and draw a map

--- Exercise1.41.5-VGA/software/exercise14_15/VGA_Screen.h
+++ Exercise1.41.5-VGA/software/exercise14_15/VGA_Screen.h
@@ -21,21 +21,40 @@ class VGA_Screen {
 public:
 	// Public Methods
 	VGA_Screen();
+
+	// initializing methods
 	void init();
+
+	// drawing functions
 	void draw_character(alt_up_char_buffer_dev* char_buffer);
 	void draw_diagonal_line(alt_up_pixel_buffer_dma_dev* pixel_buffer);
-	void draw_boxes(alt_up_pixel_buffer_dma_dev* pixel_buffer);
+	void draw_boxes(alt_up_pixel_buffer_dma_dev* pixel_buffer, int x_0, int x_1, int y_0, int y_1, int color);
+
+
+	// blanking/painting screen
 	void paint_screen(alt_up_pixel_buffer_dma_dev* pixel_buffer);
 	void clear_screen(alt_up_pixel_buffer_dma_dev* pixel_buffer);
 
+	// Drawing box from coordinate
+	void draw_box_from_coordinate(int x, int y, char c);
+
+	// MAP matrix
+	void draw_map_from_array();
+
+	// buffer variables
 	alt_up_pixel_buffer_dma_dev *pixel_buffer;
 	alt_up_char_buffer_dev *char_buffer;
+
+
+	char map[11][11];
+
 private:
 
 
 	// Private Methods
 	alt_up_pixel_buffer_dma_dev* init_pixel_buffer();
 	alt_up_char_buffer_dev* init_char_buffer();
+	void init_array();
 };
 
 #endif /* VGA_SCREEN_H_ */
--- Exercise1.41.5-VGA/software/exercise14_15/hello_world.cpp
+++ Exercise1.41.5-VGA/software/exercise14_15/hello_world.cpp
@@ -4,14 +4,12 @@
 #include <stdlib.h>
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
 #include "altera_up_avalon_video_character_buffer_with_dma.h"
-
 #include "VGA_Screen.h"
 
+using namespace std;
 int main() {
 	VGA_Screen vga_screen;
 	vga_screen.init();
-	vga_screen.paint_screen(vga_screen.pixel_buffer);
-	vga_screen.draw_boxes(vga_screen.pixel_buffer);
-	vga_screen.draw_diagonal_line(vga_screen.pixel_buffer);
-	vga_screen.draw_character(vga_screen.char_buffer);
+	vga_screen.clear_screen(vga_screen.pixel_buffer);
+	vga_screen.draw_map_from_array();
 }
