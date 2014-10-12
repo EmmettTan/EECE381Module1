173dc8d19f38fb67f4fdbfb3f8859eb57f1b2ca9
kiddo122 <kaiboma@gmail.com>
2014-10-12 14:40:12 -0700

    Revert "Revert "Revert "Can take bitmaps and draw from them, extremely slow, will have to fix"""
    This reverts commit a81c5541dfeb57af9d5c98de2a9c1498d2b1f04c.

--- module1_integration/software/Bomberman/SD_Card.h
+++ module1_integration/software/Bomberman/SD_Card.h
@@ -8,19 +8,16 @@
 #ifndef SD_CARD_H_
 #define SD_CARD_H_
 #define Bomberman_Forward_Bitmap "b_f.bmp"
-#define Speed_Powerup_Bitmap "SpeedUp.bmp"
 #include "altera_up_sd_card_avalon_interface.h"
 
 
 class SD_Card {
 public:
+	bool check_sd_card();
 	void scan_sd_card(char *directory, char *filename);
 	bool init();
 	void look_for_other_files(char *filename);
-	void change_bitmap_array_from_file(char* filename);
 
-
-	short int bitmap_array[20][20];
 	alt_up_sd_card_dev *device_reference;
 	int connected;
 	char *filename;
